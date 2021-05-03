460f2bda7267311bca4131901b8319800d8842d7
Xbulldcm <dcm2311@gmail.com>
2021-05-03 14:56:10 -0600

    AddContract

--- MyLeasing.Web/Helpers/IConverterHelper.cs
+++ MyLeasing.Web/Helpers/IConverterHelper.cs
@@ -10,8 +10,8 @@ namespace MyLeasing.Web.Helpers
 
         PropertyViewModel ToPropertyViewModel(Property property);
 
-        //Task<Contract> ToContractAsync(ContractViewModel model, bool isNew);
+        Task<Contract> ToContractAsync(ContractViewModel model, bool isNew);
 
-        //ContractViewModel ToContractViewModel(Contract contract);
+        ContractViewModel ToContractViewModel(Contract contract);
     }
 }
