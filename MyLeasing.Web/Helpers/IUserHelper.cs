efdbd53f0d5420efdc9e5fcffa0b7379192af5a5
Xbulldcm <dcm2311@gmail.com>
2021-05-03 19:03:08 -0600

    Edit Owner

--- MyLeasing.Web/Helpers/IUserHelper.cs
+++ MyLeasing.Web/Helpers/IUserHelper.cs
@@ -23,7 +23,8 @@ namespace MyLeasing.Web.Helpers
         Task<SignInResult> LoginAsync(LoginViewModel model);
 
         Task LogoutAsync();
-
+        Task<bool> DeleteUserAsync(string email);
+        Task<IdentityResult> UpdateUserAsync(User user);
     }
 }
 
